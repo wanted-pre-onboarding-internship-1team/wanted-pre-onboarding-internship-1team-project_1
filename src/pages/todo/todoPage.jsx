93609fde73502d09210ec3a53989dfa091beef64
yundonghui <qeadzc9503@naver.com>
2023-02-23 02:50:20 +0900

    refactor : 수정님이 사용하신  styled-components 라이브러리로 변경 및 react-icons 라이브러리 추가

--- src/pages/todo/todoPage.jsx
+++ src/pages/todo/todoPage.jsx
@@ -1,3 +1,17 @@
+import TodoHeader from '../../components/todo/TodoHeader';
+import TodoList from '../../components/todo/TodoList';
+
 export default function todoPage() {
-  return <>todo페이지</>;
+  return (
+    <>
+      <TodoHeader
+      // filters={FILTERS}
+      // currentFilter={currentFilter}
+      // onFilterChange={setCurrentFilter}
+      />
+      <TodoList
+      //   currentFilter={currentFilter}
+      />
+    </>
+  );
 }
