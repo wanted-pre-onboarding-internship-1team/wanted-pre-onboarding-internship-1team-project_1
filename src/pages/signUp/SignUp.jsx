c3e0dc8c6da593552a2eaf561aec2c34cba84765
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-02-23 17:04:54 +0900

    feat : 회원가입 기능 구현

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
-import FormInput from '../../components/input/FormInput';
-import FormButton from '../../components/button/SignButton';
+import React, { useState } from 'react';
+import FormInput from '../../components/common/input/FormInput';
+import FormButton from '../../components/common/button/FormButton';
 import styled from 'styled-components';
+import { api } from './../../apis/index';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const [inputValue, setInputValue] = useState({ text: '', password: '' });
+  const navigate = useNavigate();
+
+  const onChange = e => {
+    const { type, value } = e.target;
+
+    setInputValue(prevValue => ({ ...prevValue, [type]: value }));
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    const { text, password } = inputValue;
+
+    api.auth
+      .signup(text, password)
+      .then(res => {
+        alert('회원가입이 완료되었습니다 🙌');
+        navigate('/signin');
+      })
+      .catch(err => alert(err.response.data.message));
+  };
+
   return (
     <>
       <Title>이메일 주소로 가입하기</Title>
-      <FormSignUp>
+      <FormSignUp onSubmit={onSubmit}>
         <InputInfo>어떤 이메일로 가입하시겠습니까?</InputInfo>
         <FormInput
           testId='email-input'
           name='id'
           type='text'
           placeholder='internship@wanted.co.kr'
+          value={inputValue.text}
+          changeFunc={onChange}
         />
         <InputInfo>
           비밀번호를 설정해주세요. <ValidatePw>8자 이상</ValidatePw>
@@ -23,6 +50,8 @@ export default function SignUp() {
           name='pw'
           type='password'
           placeholder='비밀번호'
+          value={inputValue.text}
+          changeFunc={onChange}
         />
         <FormButton
           testId='signup-button'
