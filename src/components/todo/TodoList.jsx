293bfd85ecf415b892bd9ae6caed4484fec02bcb
shings <shings@fittingmonster.com>
2023-02-23 17:08:53 +0900

    feat : todoContext 적용

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -2,35 +2,49 @@ import React, { useEffect, useState } from 'react';
 import Todo from './Todo';
 import AddTodo from './AddTodo';
 import styled from 'styled-components';
+import useApi from '../../hooks/useApi';
+// import TodoService from '../../apis/todo';
+import { api } from '../../apis/index';
+import { useTodoContext } from '../../context/todoContext';
 
 export default function TodoList({ currentFilter }) {
-  const filteredTodos = [
-    { id: 1, todo: 'todo1' },
-    { id: 2, todo: 'todo2' },
-  ];
+  const todoApi = api.todo;
+
+  const { todos, setTodos } = useTodoContext();
+  const [{ data }] = useApi(todoApi.getTodos(), []);
+
+  useEffect(() => {
+    if (!data) return;
+    setTodos(data);
+  }, [data]);
+
+  function getFilteredItems(todos, filter) {
+    if (filter === 0) {
+      return todos;
+    } else if (filter === 1) {
+      return todos.filter(todo => todo.isCompleted === false);
+    } else {
+      return todos.filter(todo => todo.isCompleted === true);
+    }
+  }
+
+  // const filteredTodos = [
+  //   { id: 1, todo: 'todo1' },
+  //   { id: 2, todo: 'todo2' },
+  // ];
+  const filteredTodos = getFilteredItems(todos, 0);
 
   return (
     <Section>
+      <AddTodo />
       <TodoBody>
         {filteredTodos.map(todo => (
           <Todo key={todo.id} todoObj={todo} />
         ))}
       </TodoBody>
-      <AddTodo />
     </Section>
   );
 }
-
-function getFilteredItems(todos, filter) {
-  if (filter === 0) {
-    return todos;
-  } else if (filter === 1) {
-    return todos.filter(todo => todo.isCompleted === false);
-  } else {
-    return todos.filter(todo => todo.isCompleted === true);
-  }
-}
-
 const Section = styled.section`
   height: 100%;
   min-height: 0;
